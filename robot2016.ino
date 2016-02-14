15938754529e9ae1a92c53d35cd92e4ddc498f58
reneech <reneech18@gmail.com>
2016-02-14 15:01:45 -0500

    Make a wave of lights
    Set up a system where a line of lights will move forward using sin.
    Still need to add motorspeed detection and case detection.

---
+++ robot2016.ino
@@ -0,0 +1,34 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
+
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+#include "robot_state.h"
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            8
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      16
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  pixels.begin();
+}
+
+void loop() {
+ for(int j=0;j<NUMPIXELS;j++){
+   for(int i=0;i<NUMPIXELS;i++){
+    double val = (sin(j) + 1)/2;
+    val *= val;
+    pixels.setPixelColor(i, 0, 0, val*40);
+    pixels.show();
+    tick(50);
+   }
+  }
+}
